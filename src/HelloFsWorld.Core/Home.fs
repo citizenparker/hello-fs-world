10216caca0071419f0f474087cbfede012937b60
Scott Parker (citizenparker) <scott.p.parker@gmail.com>
2010-04-25 22:58:31 -0500

    Initial Commit

---
+++ src/HelloFsWorld.Core/Home.fs
@@ -0,0 +1,13 @@
+﻿module Home
+  
+  open Bistro.FS.Definitions
+  open Bistro.FS.Inference
+    
+  open Bistro.Controllers.Descriptor
+  open Bistro.Controllers.Descriptor.Data
+
+  [<Bind("")>]
+  [<RenderWith("Views/home.django")>]
+  [<ReflectedDefinition>]
+  let home (ctx : ictx) =
+    "Hello, my F# World!" |> named "message"
