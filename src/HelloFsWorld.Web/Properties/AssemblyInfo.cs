7d2a7742691ff86e976ddb86fe582329ca9d1050
Scott Parker (citizenparker) <scott.p.parker@gmail.com>
2010-04-26 07:19:28 -0500

    Cleaned up minor oddity in AssemblyInfo.cs

--- src/HelloFsWorld.Web/Properties/AssemblyInfo.cs
+++ src/HelloFsWorld.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FsChicago.Web")]
+[assembly: AssemblyTitle("HelloFsWorld.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FsChicago.Web")]
+[assembly: AssemblyProduct("HelloFsWorld.Web")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
